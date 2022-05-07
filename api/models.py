c3509c9098302f3713288ddd6f57d1c47fdbf666
ivymmurithi <ivymurithi@gmail.com>
2022-05-07 22:19:34 +0300

    set up cors

--- api/models.py
+++ api/models.py
@@ -11,3 +11,6 @@ from django.db import models
 class Account(models.Model):
     username = models.CharField(max_length=255, null=False, blank=False, unique=True)
     balance = models.FloatField(default=0, null=False, blank=False)
+
+    def __str__(self):
+        return self.username
--- kaya_challenge_api/settings.py
+++ kaya_challenge_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'kaya_challenge_api.urls'
@@ -127,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGIN_REGEXES = [r"^http.*$"]
+CORS_ALLOW_METHODS = [
+    "GET",
+    "PUT",
+]
+CORS_URLS_REGEX = r"^.*$"
