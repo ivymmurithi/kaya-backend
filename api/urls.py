d710152be534ad127d3964ae62f7a08511316257
ivymmurithi <ivymurithi@gmail.com>
2022-05-10 18:27:21 +0300

    set up static files for deployment

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from api.views import update_balance, list_accounts, get_transactions
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('update/balance/<int:account_id>', view=update_balance, name="update_balance"),
     path('', view=list_accounts, name="list_accounts"),
     path("transactions/", view=get_transactions, name="list_transactions"),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
